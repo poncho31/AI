f4fe8d700952a29469d808bc3170cd8aabdc85e7
poncho31 <gilles.pinchart@gmail.com>
2024-01-22 20:47:10 +0100

    fix comfy ui api

--- src/api/comfyui_api.php
+++ src/api/comfyui_api.php
@@ -2,7 +2,7 @@
 
 $apiUrl        = "http://127.0.0.1:7860/prompt";
 $type_api_json = "";
-$prompt        = $argv[1]??"Generate an image of a beautiful landscape bathed in the soft glow of dawn. In the foreground, a tranquil lake mirrors the warming hues of the sunrise";
+$userPrompt    = $argv[1]??"Generate an image of a beautiful landscape bathed in the soft glow of dawn. In the foreground, a tranquil lake mirrors the warming hues of the sunrise";
 $number_output = $argv[2]??1;                   // ex: 5 images eb sortie
 $type_api      = $argv[3]??"textToImageFast";
 
@@ -20,18 +20,20 @@ switch ($type_api){
         break;
 }
 print_r("API : ".basename($type_api_json) ."\r\n");
-echo "$prompt $number_output $type_api \r\n";
+echo "$userPrompt $number_output $type_api \r\n";
 
 // Va chercher la configuration pour Comfy ui
 $type_api_json = file_get_contents($type_api_json);
 
 // Modifiez le prompt comme souhaité
 $prompt = json_decode($type_api_json, true);
-$prompt["6"]["inputs"]["text"]       = $prompt;
+
+$prompt["6"]["inputs"]["text"]       = $userPrompt;
 $prompt["3"]["inputs"]["seed"]       = $type_api == "textToImageFast" ? 20 : 60;
 $prompt["5"]["inputs"]["batch_size"] = $number_output;
 
 // Effectuez la requête HTTP avec le prompt modifié
+var_dump($prompt);
 $prompt = json_encode(["prompt" => $prompt]);
 // file_put_contents("test.json", $prompt);
 $ch = curl_init($apiUrl);
