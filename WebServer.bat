3f5f91410db7ed4542a382c1401fedf239e3057d
Gilles Pinchart <gilles.pinchart@scabel.eu>
2024-01-23 10:01:46 +0100

    update webserver  add host var + browser automatic start

--- WebServer.bat
+++ WebServer.bat
@@ -1,5 +1,7 @@
 @echo off
 cd /d %~dp0
 cd packages\php\php-8.3.2-nts
-php -S localhost:666 -t . ..\..\..\public\index.php
+set "host=localhost:666"
+start "" "http://%host%"
+php -S %host% -t . ..\..\..\public\index.php
 REM php -S localhost:666 -t . ..\..\..\src\api\ollama_api.php
