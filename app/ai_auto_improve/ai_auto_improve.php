0d2535615e1c7182a2800cfe9f1ed838faf11608
poncho31 <gilles.pinchart@gmail.com>
2024-01-25 23:01:18 +0100

    update php structure + auto improve ai app

---
+++ app/ai_auto_improve/ai_auto_improve.php
@@ -0,0 +1,12 @@
+<?php
+namespace AI\app\ai_auto_improve;
+
+class ai_auto_improve{
+    public function run(){
+        $fichierServer="index.php";
+        $executeWebServer= exec("php -s locahost:6666 ");
+        $recupererLesErreursOuLeCode="";
+        $analyserSiErreurOuCodenormal="";
+
+    }
+}
